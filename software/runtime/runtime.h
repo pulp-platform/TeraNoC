oakentling/mempool

[apps] Add parametrized sequential memory region with reserved region for queues

software/runtime/runtime.h
@@ -39,11 +39,13 @@ static inline void mempool_init(const uint32_t core_id,
 
     // Initialize L1 Sequential Heap Allocator per Tile
     extern int32_t __seq_start;
-    int32_t *seq_base = &__seq_start + (4096 / 4);
-    uint32_t seq_size = 4096;
+    uint32_t seq_heap_offset = 4 * STACK_SIZE + 16 * 4 * QUEUE_SIZE;
+    uint32_t seq_total_size = 4 * SEQ_MEM_SIZE;
+    int32_t *seq_heap_base = &__seq_start + (seq_heap_offset / 4);
+    uint32_t seq_heap_size = seq_total_size - seq_heap_offset;
     for (uint32_t tile_id = 0; tile_id < num_cores / 4; ++tile_id) {
-      alloc_init(get_alloc_tile(tile_id), seq_base, seq_size);
-      seq_base += (8192 / 4);
+      alloc_init(get_alloc_tile(tile_id), seq_heap_base, seq_heap_size);
+      seq_heap_base += (seq_total_size / 4);
     }
   }
 }

software/runtime/runtime.mk
@@ -61,6 +61,7 @@ RISCV_STRIP   ?= $(RISCV_PREFIX)strip
 # Defines
 DEFINES += -DPRINTF_DISABLE_SUPPORT_FLOAT -DPRINTF_DISABLE_SUPPORT_LONG_LONG -DPRINTF_DISABLE_SUPPORT_PTRDIFF_T
 DEFINES += -DNUM_CORES=$(num_cores) -DNUM_GROUPS=$(num_groups) -DBOOT_ADDR=0x$(boot_addr) -DL2_BASE=0x$(l2_base) -DL2_SIZE=0x$(l2_size)
+DEFINES += -DSEQ_MEM_SIZE=$(seq_mem_size) -DSTACK_SIZE=$(stack_size) -DQUEUE_SIZE=$(queue_size)
 
 # Specify cross compilation target. This can be omitted if LLVM is built with riscv as default target
 RISCV_LLVM_TARGET  ?= --target=$(RISCV_TARGET) --sysroot=$(GCC_INSTALL_DIR)/$(RISCV_TARGET) --gcc-toolchain=$(GCC_INSTALL_DIR)

pulp-platform/TeraNoC

software/runtime/runtime.h
@@ -39,11 +39,13 @@ static inline void mempool_init(const uint32_t core_id,
 
     // Initialize L1 Sequential Heap Allocator per Tile
     extern int32_t __seq_start;
-    int32_t *seq_base = &__seq_start + (4096 / 4);
-    uint32_t seq_size = 4096;
+    uint32_t seq_heap_offset = 4 * STACK_SIZE + 16 * 4 * XQUEUE_SIZE;
+    uint32_t seq_total_size = 4 * SEQ_MEM_SIZE;
+    int32_t *seq_heap_base = &__seq_start + (seq_heap_offset / 4);
+    uint32_t seq_heap_size = seq_total_size - seq_heap_offset;
     for (uint32_t tile_id = 0; tile_id < num_cores / 4; ++tile_id) {
-      alloc_init(get_alloc_tile(tile_id), seq_base, seq_size);
-      seq_base += (8192 / 4);
+      alloc_init(get_alloc_tile(tile_id), seq_heap_base, seq_heap_size);
+      seq_heap_base += (seq_total_size / 4);
     }
   }
 }

software/runtime/runtime.mk
@@ -61,6 +61,7 @@ RISCV_STRIP   ?= $(RISCV_PREFIX)strip
 # Defines
 DEFINES += -DPRINTF_DISABLE_SUPPORT_FLOAT -DPRINTF_DISABLE_SUPPORT_LONG_LONG -DPRINTF_DISABLE_SUPPORT_PTRDIFF_T
 DEFINES += -DNUM_CORES=$(num_cores) -DNUM_GROUPS=$(num_groups) -DBOOT_ADDR=0x$(boot_addr) -DL2_BASE=0x$(l2_base) -DL2_SIZE=0x$(l2_size)
+DEFINES += -DSEQ_MEM_SIZE=$(seq_mem_size) -DSTACK_SIZE=$(stack_size) -DXQUEUE_SIZE=$(xqueue_size)
 
 # Specify cross compilation target. This can be omitted if LLVM is built with riscv as default target
 RISCV_LLVM_TARGET  ?= --target=$(RISCV_TARGET) --sysroot=$(GCC_INSTALL_DIR)/$(RISCV_TARGET) --gcc-toolchain=$(GCC_INSTALL_DIR)