009ab969851319d25edb7c9a63b855278e2cd96c
Diyou Shen <dishen@iis.ee.ethz.ch>
2025-10-26 09:27:13 +0100

    [SW] Continue to adapt runtime

--- software/runtime/runtime.h
+++ software/runtime/runtime.h
@@ -12,7 +12,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define NUM_BANKS_PER_TILE NUM_CORES_PER_TILE *BANKING_FACTOR
+#define NUM_BANKS_PER_TILE NUM_CORES_PER_TILE * BANKING_FACTOR * N_FU
 
 extern char l1_alloc_base;
 static uint32_t volatile *wake_up_reg =
